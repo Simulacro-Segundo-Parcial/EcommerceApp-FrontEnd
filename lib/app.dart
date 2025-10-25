275df4348faeed258661f38bea0b2f0228d49e3d
Carlosmzh <a2024112447@estudiantes.upsa.edu.bo>
2025-10-25 10:33:51 -0400

    First Commit App Base Flutter

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,9 @@
 // lib/app.dart
 import 'package:flutter/material.dart';
 import 'core/config/app_config.dart';
+import 'core/services/secure_storage.dart';
+import 'core/services/network_client.dart';
+import 'core/routing/app_router.dart';
 import 'core/theme/app_theme.dart';
 
 class MyApp extends StatelessWidget {
@@ -9,17 +12,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Inicializamos los servicios base
+    final storage = SecureStorage();
+    // ignore: unused_local_variable
+    final client = NetworkClient(
+      baseUrl: config.baseUrl,
+      getToken: storage.getToken,
+    );
+
     return MaterialApp(
       title: 'Ecommerce',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Ecommerce Base')),
-        body: Center(
-          child: Text('Base URL: ${config.baseUrl}'),
-        ),
-      ),
+      initialRoute: AppRouter.splash,
+      onGenerateRoute: AppRouter.generateRoute,
     );
   }
 }
